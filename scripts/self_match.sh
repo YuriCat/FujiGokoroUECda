624fb7b6ed395c3376404b6aa4f9c26a78902479
YuriCat <a.a.b.a.b.c.a.b.c.d.abcd1234@gmail.com>
2017-07-15 17:42:48 +0900

    設定ファイル内容変更

--- scripts/self_match.sh
+++ scripts/self_match.sh
@@ -4,11 +4,11 @@ port=${3}
 cd ..
 mkdir ./dump/
 echo ${name} ${index} ${port}
-./out/release/server -l ./dump/record_${name}${index}.dat -g 10000 -p ${port} 1>>./dump/output_server_${name}${index}.txt &
+./out/release/server -l ./dump/record_${name}_${index}.dat -g 10000 -p ${port} 1>>./dump/output_server_${name}_${index}.txt &
 sleep 2.5
-./out/release/client -p ${port} 2>>./dump/output_client${name}${index}_0.txt &
-./out/release/client -p ${port} 2>>./dump/output_client${name}${index}_1.txt &
-./out/release/client -p ${port} 2>>./dump/output_client${name}${index}_2.txt &
-./out/release/client -p ${port} 2>>./dump/output_client${name}${index}_3.txt &
-./out/release/client -p ${port} 2>>./dump/output_client${name}${index}_4.txt
+./out/release/client -p ${port} 2>>./dump/output_client${name}_${index}_0.txt &
+./out/release/client -p ${port} 2>>./dump/output_client${name}_${index}_1.txt &
+./out/release/client -p ${port} 2>>./dump/output_client${name}_${index}_2.txt &
+./out/release/client -p ${port} 2>>./dump/output_client${name}_${index}_3.txt &
+./out/release/client -p ${port} 2>>./dump/output_client${name}_${index}_4.txt
 cd scripts
