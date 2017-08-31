70b507074b8dc88e84d3bb597c8de0b666523ec0
YuriCat <a.a.b.a.b.c.a.b.c.d.abcd1234@gmail.com>
2017-08-31 21:55:42 +0900

    self_matchはteacherビルドが基本とした

--- scripts/self_match.sh
+++ scripts/self_match.sh
@@ -6,9 +6,9 @@ mkdir ./dump/
 echo ${name} ${index} ${port}
 ./out/release/server -l ./dump/record_${name}_${index}.dat -g 10000 -p ${port} 1>>./dump/output_server_${name}_${index}.txt &
 sleep 2.5
-./out/release/client -p ${port} 2>>./dump/output_client${name}_${index}_0.txt &
-./out/release/client -p ${port} 2>>./dump/output_client${name}_${index}_1.txt &
-./out/release/client -p ${port} 2>>./dump/output_client${name}_${index}_2.txt &
-./out/release/client -p ${port} 2>>./dump/output_client${name}_${index}_3.txt &
-./out/release/client -p ${port} 2>>./dump/output_client${name}_${index}_4.txt
+./out/teacher/client -p ${port} 2>>./dump/output_client${name}_${index}_0.txt &
+./out/teacher/client -p ${port} 2>>./dump/output_client${name}_${index}_1.txt &
+./out/teacher/client -p ${port} 2>>./dump/output_client${name}_${index}_2.txt &
+./out/teacher/client -p ${port} 2>>./dump/output_client${name}_${index}_3.txt &
+./out/teacher/client -p ${port} 2>>./dump/output_client${name}_${index}_4.txt
 cd scripts
