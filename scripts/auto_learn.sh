0f943557ac08a79a75f932d3f985a3ccdee57a0c
YuriCat <a.a.b.a.b.c.a.b.c.d.abcd1234@gmail.com>
2019-04-01 18:06:02 +0900

    auto_learn.shを追加

---
+++ scripts/auto_learn.sh
@@ -0,0 +1,26 @@
+time=`date '+%s'`
+echo ${time}
+
+# 設定
+auto_dir=
+learn_record_dir=
+
+cd ..
+
+# 保存ディレクトリ作成
+save_dir=${auto_dir}${time}/
+mkdir -p ${save_dir}
+
+backup_dir=${save_dir}backup/
+save_param_dir=${save_dir}param/
+mkdir -p ${backup_dir}
+mkdir -p ${save_param_dir}
+
+# ビルド
+make release -j4
+
+# データのバックアップを取る
+zip -r ${backup_dir}uecda.zip .
+
+# 学習
+./out/release/policy_learner -o ${save_param_dir} -th 7 -t -i 1000 -ld ${learn_record_dir} 2>${save_dir}policy_learn_log.txt
