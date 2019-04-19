25f4ec3305b3c6cb5a278a7031481e6b059dc5a9
YuriCat <a.a.b.a.b.c.a.b.c.d.abcd1234@gmail.com>
2019-04-20 04:26:14 +0900

    Selectorの0チェック

--- src/base/util.hpp
+++ src/base/util.hpp
@@ -883,9 +883,10 @@ struct StochasticSelector {
     score_(ascore), size_(asize), sum_(0) {}
 
     double prob(int i) const {
-        return score_[i] / sum_;
+        return sum_ == 0 ? 1.0 / size_ : score_[i] / sum_;
     }
     int select(double urand) const {
+        if (sum_ == 0) return int(urand * size_);
         double r = urand * sum_;
         int i = 0;
         for (; i < size_ - 1; i++) {
@@ -912,6 +913,7 @@ struct SoftmaxSelector : public StochasticSelector<T> {
     base_t(ascore, asize) {
         for (int i = 0; i < asize; i++) {
             double es = std::exp(base_t::score_[i] / atemp);
+            if (std::isinf(es)) getchar();
             base_t::score_[i] = es;
             base_t::sum_ += es;
         }
