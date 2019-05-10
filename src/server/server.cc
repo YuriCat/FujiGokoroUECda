9af10f6d778511ccfa530b35905b7a8427223fa5
YuriCat <a.a.b.a.b.c.a.b.c.d.abcd1234@gmail.com>
2019-05-10 20:42:41 +0900

    server.cc警告修正

--- src/server/server.cc
+++ src/server/server.cc
@@ -845,7 +845,7 @@ int main(int argc, char *argv[]) {
                     default:
                         break;
                 }
-            } else if ((error==0)) {
+            } else if (error==0) {
                 switch (status_of_submitted_card[3]) {
                     case 1:
                         break;
