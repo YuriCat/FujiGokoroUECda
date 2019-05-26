82e48fbe4f1d52e8cbfa17eb4eaa4e2333bccfc0
YuriCat <a.a.b.a.b.c.a.b.c.d.abcd1234@gmail.com>
2019-05-27 00:12:23 +0900

    engine.hpp微修正

--- src/engine/engine.hpp
+++ src/engine/engine.hpp
@@ -193,7 +193,6 @@ public:
         const auto& gameLog = shared.record.latestGame();
         
         Move playMove = MOVE_NONE;
-        
         RootInfo root;
         
         // ルート合法手生成バッファ
@@ -204,15 +203,13 @@ public:
         Field field;
         setFieldFromClientLog(gameLog, myPlayerNum, &field);
         field.setMoveBuffer(mbuf.data());
-        DERR << "tp = " << gameLog.current.turn() << endl;
-        ASSERT_EQ(field.turn(), myPlayerNum);
+        assert(field.turn() == myPlayerNum);
         
         const Hand& myHand = field.getHand(myPlayerNum);
         const Hand& opsHand = field.getOpsHand(myPlayerNum);
         const Cards myCards = myHand.cards;
         const Cards opsCards = opsHand.cards;
         const Board b = field.board;
-        CERR << b << endl;
         FieldAddInfo& fieldInfo = field.fieldInfo;
         
         // サーバーの試合進行バグにより無条件支配役が流れずに残っている場合はリジェクトにならないようにパスしておく
