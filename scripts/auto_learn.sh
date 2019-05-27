2c8a523a0e49fcd7b13e85eb87fca670158a695c
YuriCat <a.a.b.a.b.c.a.b.c.d.abcd1234@gmail.com>
2019-05-27 17:55:53 +0900

    学習スクリプトのズレを修正

--- scripts/auto_learn.sh
+++ scripts/auto_learn.sh
@@ -5,8 +5,6 @@ echo ${time}
 auto_dir=
 learn_record_dir=
 
-cd ..
-
 # 保存ディレクトリ作成
 save_dir=${auto_dir}${time}/
 mkdir -p ${save_dir}
@@ -17,10 +15,10 @@ mkdir -p ${backup_dir}
 mkdir -p ${save_param_dir}
 
 # ビルド
-make release -j4
+make -j4
 
 # データのバックアップを取る
 zip -r ${backup_dir}uecda.zip .
 
 # 学習
-./out/release/policy_learner -o ${save_param_dir} -th 7 -t -i 1000 -ld ${learn_record_dir} 2>${save_dir}policy_learn_log.txt
+./out/learner -o ${save_param_dir} -th 8 -t -i 1000 -ld ${learn_record_dir} 2>${save_dir}policy_learn_log.txt
