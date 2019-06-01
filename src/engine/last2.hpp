26622ecde8d82ba5bc6e0a36057cad1400318d24
YuriCat <a.a.b.a.b.c.a.b.c.d.abcd1234@gmail.com>
2019-06-01 21:06:16 +0900

    2人探索デバッグ出力を消してコード整理

--- src/engine/last2.hpp
+++ src/engine/last2.hpp
@@ -3,7 +3,6 @@
 #include "../core/daifugo.hpp"
 #include "../core/prim2.hpp"
 #include "../core/hand.hpp"
-#include "mate.hpp"
 
 // ラスト二人探索
 
@@ -19,21 +18,15 @@ enum L2Result {
 struct L2Field;
 
 class L2Judge {
-    
-    // L2判定人
-    // 単純な構造でデータを受け取り、
-    
+    // L2判定
     // judge...局面L2判定を返す
     // check...着手L2判定を返す
     // search...与えられた着手集合に勝利着手があればインデックス、なければ-1を返す
-    
-private:
+
+protected:
     const int NODE_LIMIT;
-    
     MoveInfo *const mbuf;
-    
-    // 統計情報
-    int nodes, childs, failed;
+    int nodes, childs, failed; // 統計情報
     
 public:
     void init() {
@@ -42,13 +35,12 @@ public:
     
     L2Judge(int nl, MoveInfo *const mb):
     NODE_LIMIT(nl), mbuf(mb) { init(); }
-    ~L2Judge() {}
 
     // 再帰版
     template <int S_LEVEL, int E_LEVEL>
     int judge(const int depth, MoveInfo *const buf, const Hand& myHand, const Hand& opsHand, const L2Field& field);
     int check(const int depth, MoveInfo *const buf, MoveInfo& tmp, const Hand& myHand, const Hand& opsHand, const L2Field& field, bool checkedEasy = false);
-    int checkDomMate(const int depth, MoveInfo *const buf, MoveInfo& tmp, const Hand& myHand, const Hand& opsHand, const L2Field& field);
+    bool checkDomMate(const int depth, MoveInfo *const buf, MoveInfo& tmp, const Hand& myHand, const Hand& opsHand, const L2Field& field);
     int search(const int depth, MoveInfo *const buf, const int NMoves, const Hand& myHand, const Hand& opsHand, const L2Field& field);
     
     int start_judge(const Hand& myHand, const Hand& opsHand, const Board b, const FieldAddInfo fInfo);
