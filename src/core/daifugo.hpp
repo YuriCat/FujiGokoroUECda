2065b55db6ace35111102c2ecad24a08731a31a9
YuriCat <a.a.b.a.b.c.a.b.c.d.abcd1234@gmail.com>
2023-10-14 07:51:17 +0900

    canMakeGroupの論理演算変更

--- src/core/daifugo.hpp
+++ src/core/daifugo.hpp
@@ -767,11 +767,11 @@ inline bool canMakeGroup(BitCards c, int n) {
         c = CardsToQR(c);
         if (c & PQR_234) { // 2枚以上
             if (n == 2) return true;
-            if (c & PQR_34) { // 4枚
-                if (n <= 4) return true;
+            if (c & PQR_13) { // 3枚以上
+                if (n == 3) return true;
             } else {
-                if (((c & PQR_2) >> 1) & c) { // 3枚
-                    if (n == 3) return true;
+                if (c & PQR_3) { // 4枚
+                    if (n == 4) return true;
                 }
             }
         }
